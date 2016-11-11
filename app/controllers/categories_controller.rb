1c1ec498ad059c114d25559caae95332d118a1de
Rebecca Shifflett <rebeccashifflett@Rebeccas-MacBook-Pro-2.local>
2016-11-11 16:59:10 -0700

    add cat controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,48 @@
 class CategoriesController < ApplicationController
+before_action :set_category, only [:edit, :update, :show, :destroy]
+
   def index
+    @categories = Category.all
   end
 
   def show
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:style)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
